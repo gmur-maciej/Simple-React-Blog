0fbaa43a42a46f2020dee0b12652588ea498cf37
CloudfreundMaciek <88267476+florker999@users.noreply.github.com>
2025-05-26 18:21:14 +0200

    [mod] PostGallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Center, Heading, Stack } from '@chakra-ui/react'
-import './App.css'
 import { Outlet } from 'react-router'
 
 function App() {
 
   return (
     <Center>
-      <Stack align={'start'} maxW={700}>
+      <Stack maxW={700}>
         <Heading size={'4xl'} m={'0 auto'}>Blog</Heading>
         <Outlet />
       </Stack>
--- src/components/ui/postsGallery/postsGallery.tsx
+++ src/components/ui/postsGallery/postsGallery.tsx
@@ -1,4 +1,4 @@
-import { For } from "@chakra-ui/react";
+import { For, Stack } from "@chakra-ui/react";
 import Post from "../post/post";
 import React from "react";
 import postsContext from "../../../contexts/postsContext";
@@ -7,12 +7,14 @@ import { NavLink } from "react-router";
 export default function PostsGallery() {
     const posts = React.useContext(postsContext);
     return (
-        <For each={posts}>
-            {(post) => (
-                <NavLink to={'/post/' + post.id}>
-                    <Post post={post} asPreview />
-                </NavLink>
-            )}
-        </For>
+        <Stack>
+            <For each={posts}>
+                {(post) => (
+                    <NavLink to={'/post/' + post.id}>
+                        <Post post={post} asPreview />
+                    </NavLink>
+                )}
+            </For>
+        </Stack>
     )
 }
