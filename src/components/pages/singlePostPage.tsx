117f944cd20b7fb0e6af838e35599087b6a60b08
CloudfreundMaciek <88267476+florker999@users.noreply.github.com>
2025-05-27 06:35:07 +0200

    [add] Post actions bar
    - comments
    - likes

--- src/components/pages/singlePostPage.tsx
+++ src/components/pages/singlePostPage.tsx
@@ -2,15 +2,33 @@ import React from "react";
 import { useParams } from "react-router";
 import postsContext from "../../contexts/postsContext";
 import Post from "../ui/post/post";
+import PostActionsBox from "../ui/postActionsBox/postActionsBox";
+import { VStack } from "@chakra-ui/react";
 
 export default function SinglePostPage() {
     const posts = React.useContext(postsContext);
+    const [isLiked, setIsLiked] = React.useState(false);
+    const [likesNumber, setLikesNumber] = React.useState(5);
     const { id } = useParams<{ id: string }>();
     const post = posts?.find(p => p.id === id);
-
+    const like = () => {
+        setIsLiked(value => !value);
+        if (isLiked)
+            setLikesNumber(likesNumber - 1);
+        else
+            setLikesNumber(likesNumber + 1);
+    }
     if (post) {
         return (
-            <Post post={post} />
+            <VStack align={'stretch'}>
+                <Post post={post} />
+                <PostActionsBox
+                    isLiked={isLiked}
+                    likesNumber={likesNumber}
+                    sendComment={text => console.log(text)}
+                    like={like}
+                />
+            </VStack>
         )
     } else return (
         'Something wrong'
---
+++ src/components/ui/postActionsBox/postActionsBox.tsx
@@ -0,0 +1,55 @@
+import { Box, Field, Flex, IconButton, Input, Show, Stat } from "@chakra-ui/react";
+import React from "react";
+import { IoRose, IoRoseOutline, IoSend } from "react-icons/io5";
+
+interface IProps {
+    isLiked: boolean,
+    likesNumber: number,
+    like(): any,
+    sendComment(text: string): any,
+}
+
+export default function PostActionsBox(props: IProps) {
+    const commentFieldRef = React.useRef<HTMLInputElement>(null);
+    const { isLiked, likesNumber } = props;
+    const sendComment = () => {
+        if (commentFieldRef.current) {
+            props.sendComment(commentFieldRef.current.value);
+        }
+    };
+    return (
+        <Flex m={'0 8px'}>
+            <Stat.Root justifyContent={'center'}>
+                <Stat.ValueText >{likesNumber}</Stat.ValueText>
+            </Stat.Root>
+            <IconButton
+                onClick={props.like}
+                variant={'plain'}
+                _hover={{ color: 'red.700' }}
+            >
+                <Show when={!isLiked}>
+                    <IoRoseOutline />
+                </Show>
+                <Show when={isLiked}>
+                    <Box color={'red.700'}>
+                        <IoRose />
+                    </Box>
+                </Show>
+            </IconButton>
+            <Field.Root>
+                <Input
+                    variant={'outline'}
+                    ref={commentFieldRef}
+                    placeholder={"Comment..."}
+                />
+            </Field.Root>
+            <IconButton
+                onClick={sendComment}
+                variant={'plain'}
+                _hover={{ color: 'red.700' }}
+            >
+                <IoSend />
+            </IconButton>
+        </Flex>
+    )
+}
